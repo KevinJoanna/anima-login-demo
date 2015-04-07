fa4d33484262e99a6cbfb05fe60d43040e5d0ab2
qiuhd2015 <287809019@qq.com>
2015-04-07 19:28:57 +0800

    Initial commit

---
+++ gate/src/main/java/org/hdl/anima/demo/simple/gate/AppStart.java
@@ -0,0 +1,11 @@
+package org.hdl.anima.demo.simple.gate;
+
+import org.hdl.anima.Application;
+
+public class AppStart {
+
+	public static void main(String[] args) {
+		Application create = Application.create();
+		create.start();
+	}
+}
